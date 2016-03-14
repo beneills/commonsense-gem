14bfe6e09ac3dd06b147020ab0b4bb37ba67e8fc
Ben Eills <ben@beneills.com>
2016-03-14 17:04:32 +0200

    Test and implement first version

--- lib/commonsense.rb
+++ lib/commonsense.rb
@@ -1,5 +1,79 @@
+require "commonsense/basic_english"
 require "commonsense/version"
 
 module Commonsense
-  # Your code goes here...
+
+  HEADING_START_CODEPOINTS = "a-zA-Z0-9"
+  HEADING_BODY_CODEPOINTS  = "a-zA-Z0-9 "
+  HEADING_END_CODEPOINTS   = "a-zA-Z0-9"
+
+  SENTENCE_START_CODEPOINTS = "A-Z0-9"
+  SENTENCE_BODY_CODEPOINTS  = "a-zA-Z0-9 "
+  SENTENCE_END_CODEPOINTS   = "a-zA-Z0-9"
+
+  SPACE_CODEPOINT           = " "
+  PERIOD_CODEPOINT          = "\\."
+  NEWLINE_CODEPOINT         = "\n"
+
+  LINE_WHITELIST            = "a-zA-Z0-9 \\."
+  TEXT_WHITELIST            = "a-zA-Z0-9 \\.\n"
+
+  ##
+  # Tests whether line conforms to the commonsense specification of a line.
+  #
+  # +line+ should be a UTF-8 encoded Ruby string with no newline characters.
+
+  def self.valid_line?(line)
+
+      # line should be UTF-8 encoded
+      raise ArgumentError, 'line not UTF-8 encoded'    unless line.encoding == Encoding::UTF_8
+      raise ArgumentError, 'line has invalid encoding' unless line.valid_encoding?
+
+      # line should contain only whitelisted codepoints
+      return false unless /^[#{LINE_WHITELIST}]*$/.match line
+
+      # a line should be:
+      #   i)  a bare heading, or
+      #   ii) a single-space-separated list of period-terminated sentences
+      heading                    = "[#{HEADING_START_CODEPOINTS}]([#{HEADING_BODY_CODEPOINTS}]*[#{HEADING_END_CODEPOINTS}])?"
+      sentence                   = "[#{SENTENCE_START_CODEPOINTS}]([#{SENTENCE_BODY_CODEPOINTS}]*[#{SENTENCE_END_CODEPOINTS}])?#{PERIOD_CODEPOINT}"
+      sentences                  = "(#{sentence}#{SPACE_CODEPOINT})*#{sentence}"
+      return false unless /^#{heading}$/.match line or /^#{sentences}$/.match line
+
+      # we must never have two spaces in a row
+      return false if /(#{SPACE_CODEPOINT}#{SPACE_CODEPOINT}+)/.match line
+
+      # split heading or sentence line into indidicual sentences
+      sentences = line.split(".").map { |sentence| sentence.split SPACE_CODEPOINT }
+
+      sentences.each do |sentence|
+        # first word of each sentence/heading should be in wordlist, possibly with de-capitalization
+        return false unless Commonsense::BasicEnglish.fuzzy_valid? sentence.first
+
+        # other words should be in wordlist as-is
+        return false unless sentence[1..-1].all? { |word| Commonsense::BasicEnglish.valid? word }
+      end
+
+      # if the text passes all the above, it conforms to the spec
+      return true
+  end
+
+  ##
+  # Tests whether text conforms to the commonsense specification of a multi-line text.
+  #
+  # +text+ should be a UTF-8 encoded Ruby string.
+
+  def self.valid?(text)
+
+    # text should be UTF-8 encoded
+    raise ArgumentError, 'text not UTF-8 encoded'    unless text.encoding == Encoding::UTF_8
+    raise ArgumentError, 'text has invalid encoding' unless text.valid_encoding?
+
+    # text should contain only whitelisted codepoints
+    return false unless /^[#{TEXT_WHITELIST}]*$/.match text
+
+    # text should be a sequence of zero or more lines
+    return text.split(NEWLINE_CODEPOINT).all? { |line| line.empty? or valid_line? line }
+  end
+
 end
--- test/commonsense_test.rb
+++ test/commonsense_test.rb
@@ -1,11 +1,78 @@
 require 'test_helper'
 
 class CommonsenseTest < Minitest::Test
+
   def test_that_it_has_a_version_number
     refute_nil ::Commonsense::VERSION
   end
 
-  def test_it_does_something_useful
-    assert false
+  def test_valid_texts
+    # trivial texts
+    assert ::Commonsense::valid? ""
+
+    # single-line headings
+    assert ::Commonsense::valid? "writing"
+    assert ::Commonsense::valid? "Protest"
+    assert ::Commonsense::valid? "writing against"
+    assert ::Commonsense::valid? "Protest against"
+
+    # single-line sentences
+    assert ::Commonsense::valid? "Writing against government."
+    assert ::Commonsense::valid? "Writing against government. Free talk be healthy."
+
+    # multi-line texts
+    assert ::Commonsense::valid? "writing\nagainst\n\ngovernment"
+    assert ::Commonsense::valid? "Protest\n\nWriting against government. Free talk be healthy.\n\norganization"
+
+    # Edge-case texts
+    assert ::Commonsense::valid? "I"
+    assert ::Commonsense::valid? "I.\nA. A."
+    assert ::Commonsense::valid? "You and I."
   end
+
+  def test_bad_characters
+    refute ::Commonsense::valid? "writing $ be bad"
+    refute ::Commonsense::valid? "_"
+    refute ::Commonsense::valid? "-"
+    refute ::Commonsense::valid? "bad\r"
+    refute ::Commonsense::valid? "bad\t"
+  end
+
+  def test_bad_words
+    refute ::Commonsense::valid? "agitprop"
+    refute ::Commonsense::valid? "Obtuse. Mutterings."
+  end
+
+  def test_bad_capitalization
+    refute ::Commonsense::valid? "Bad Writing"
+    refute ::Commonsense::valid? "bad Writing"
+    refute ::Commonsense::valid? "bad writing."
+    refute ::Commonsense::valid? "bAd"
+    refute ::Commonsense::valid? "bAd."
+  end
+
+  def test_bad_capitalization
+    refute ::Commonsense::valid? "Bad Writing"
+    refute ::Commonsense::valid? "bad Writing"
+    refute ::Commonsense::valid? "bad writing."
+    refute ::Commonsense::valid? "bAd"
+    refute ::Commonsense::valid? "bAd."
+  end
+
+  def test_bad_spacing
+    refute ::Commonsense::valid? " bad space"
+    refute ::Commonsense::valid? "bad space "
+    refute ::Commonsense::valid? " Bad space."
+    refute ::Commonsense::valid? "Bad space. "
+    refute ::Commonsense::valid? "Bad  space."
+    refute ::Commonsense::valid? "Bad space.  Bad."
+    refute ::Commonsense::valid? "Bad  space.\n "
+    refute ::Commonsense::valid? "Bad space.\n Bad."
+  end
+
+  def test_bad_periods
+    refute ::Commonsense::valid? ".."
+    refute ::Commonsense::valid? "Writing.."
+  end
+
 end
